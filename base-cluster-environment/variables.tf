bac8e4066313ab6ba4fe77d3118b5c35a1d8e114
Owain Kenway <o.kenway@ucl.ac.uk>
2024-09-03 11:31:42 +0100

    Terraform and ansible to build a slurm cluster with NFS

---
+++ base-cluster-environment/variables.tf
@@ -0,0 +1,25 @@
+variable img_display_name {
+  type = string
+  default = "almalinux-9.4-20240805"
+}
+
+variable namespace {
+  type = string
+  default = "arc-general-ns"
+}
+
+variable network_name {
+  type = string
+  default = "arc-general-ns/gen-proj"
+}
+
+variable username {
+  type = string
+#  default = ""
+}
+
+
+variable vm_count {
+  type    = number
+  default = 4
+}
