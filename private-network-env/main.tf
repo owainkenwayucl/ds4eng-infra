66b747d806699fdf6fb1c8e76a91da37d799118c
Owain Kenway <o.kenway@ucl.ac.uk>
2024-09-26 14:43:15 +0100

    Gateway conf

--- private-network-env/main.tf
+++ private-network-env/main.tf
@@ -26,6 +26,21 @@ resource "harvester_cloudinit_secret" "cloud-config" {
     })
 }
 
+resource "harvester_cloudinit_secret" "cloud-config-gateway" {
+  count = 1
+
+
+  name      = "cloud-config-gateway-${random_id.secret.hex}-${count.index}"
+  namespace = var.namespace
+
+  user_data = templatefile("cloud-init-gateway.tmpl.yml", {
+      public_key_openssh = data.harvester_ssh_key.mysshkey.public_key
+      host_ip = "${var.ip_block}.${(count.index + 1 + var.ip_offset)}"
+      gateway = "${var.ip_block}.1"
+    })
+}
+
+
 resource "harvester_virtualmachine" "vm" {
   
   count = var.vm_count
@@ -66,6 +81,50 @@ resource "harvester_virtualmachine" "vm" {
   }
 
   cloudinit {
-    user_data_secret_name = "cloud-config-${random_id.secret.hex}-${count.index}"
+    user_data_secret_name = harvester_cloudinit_secret.cloud-config.name
+  }
+}
+
+resource "harvester_virtualmachine" "vm" {
+  
+  count = 1
+
+  name                 = "${var.username}-gw-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  namespace            = var.namespace
+  restart_after_update = true
+
+  description = "Gateway VM"
+
+  cpu    = 2
+  memory = "16Gi"
+
+  efi         = true
+  secure_boot = true
+
+  run_strategy    = "RerunOnFailure"
+  hostname        = "${var.username}-gw-${format("%02d", count.index + 1)}-${random_id.secret.hex}"
+  reserved_memory = "100Mi"
+  machine_type    = "q35"
+
+  network_interface {
+    name           = "nic-1"
+    wait_for_lease = true
+    type           = "bridge"
+    network_name   = var.network_name
+  }
+
+  disk {
+    name       = "rootdisk"
+    type       = "disk"
+    size       = "50Gi"
+    bus        = "virtio"
+    boot_order = 1
+
+    image       = data.harvester_image.img.id
+    auto_delete = true
+  }
+
+  cloudinit {
+    user_data_secret_name = harvester_cloudinit_secret.cloud-config-gateway.name
   }
 }
