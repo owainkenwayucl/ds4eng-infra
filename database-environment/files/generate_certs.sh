5966eed3b1c7f5187156a8dc7878cbaf5a88abef
Owain Kenway <o.kenway@ucl.ac.uk>
2025-02-24 15:10:44 +0000

    Force tls

--- database-environment/files/generate_certs.sh
+++ database-environment/files/generate_certs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl req -x509 -newkey rsa:4096 -keyout /var/lib/pgsql/server.key -out /var/lib/pgsql/server.crt -sha256 -days 3650 -nodes -subj "/CN=${1}" -addext "subjectAltName = IP:${1}"
+openssl req -x509 -newkey rsa:4096 -keyout /var/lib/pgsql/data/server.key -out /var/lib/pgsql/data/server.crt -sha256 -days 3650 -nodes -subj "/CN=${1}" -addext "subjectAltName = IP:${1}"
