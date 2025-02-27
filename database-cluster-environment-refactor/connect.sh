268c4c36085718dfbfe75cd789a522c0cb04d4a7
Owain Kenway <o.kenway@ucl.ac.uk>
2025-02-27 16:36:42 +0000

    Many syntax fixes

--- database-cluster-environment-refactor/connect.sh
+++ database-cluster-environment-refactor/connect.sh
@@ -4,11 +4,11 @@ directory=$(dirname "$(realpath $0)")
 c_directory=$(pwd)
 
 export PGSSLMODE="verify-full"
-export PGSSLROOTCERT="${directory}/generated/server.crt"
 export PGPASSWORD=$(cat ${directory}/.postgrespass)
 
 cd ${directory}
-server=$(terraform output -json | jq -c -r .vm_ips.value[0])
+server=$(terraform output -json | jq -c -r .primary_ips.value[0])
 cd ${c_directory}
+export PGSSLROOTCERT="${directory}/generated/${server}_server.crt"
 
 psql -h ${server} "$@"
